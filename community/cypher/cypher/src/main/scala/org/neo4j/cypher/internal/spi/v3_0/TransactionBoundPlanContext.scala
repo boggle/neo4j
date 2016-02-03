c4fcd65a9d9814fd8413695ec1c3bd1daefac8a5
Stefan Plantikow <stefan.plantikow@googlemail.com>
2016-02-03 13:10:41 +0100

    Move ProcedureSignature and friends over to frontend (we need it during semantic checking)

--- community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/spi/v3_0/TransactionBoundPlanContext.scala
+++ community/cypher/cypher/src/main/scala/org/neo4j/cypher/internal/spi/v3_0/TransactionBoundPlanContext.scala
@@ -19,6 +19,7 @@
  */
 package org.neo4j.cypher.internal.spi.v3_0
 
+import org.neo4j.cypher.internal.frontend.v3_0.spi._
 import org.neo4j.cypher.{CypherExecutionException, MissingIndexException}
 import org.neo4j.cypher.internal.LastCommittedTxIdProvider
 import org.neo4j.cypher.internal.compiler.v3_0.pipes.EntityProducer
@@ -133,9 +134,9 @@ class TransactionBoundPlanContext(statement: Statement, val gdb: GraphDatabaseQu
     ProcedureSignature(name, input, output, mode)
   }
 
-  private def asCypherProcMode(mode: KernelProcedureSignature.Mode): ProcedureCallMode = mode match {
-    case KernelProcedureSignature.Mode.READ_ONLY => LazyReadOnlyCallMode
-    case KernelProcedureSignature.Mode.READ_WRITE => EagerReadWriteCallMode
+  private def asCypherProcMode(mode: KernelProcedureSignature.Mode): ProcedureMode = mode match {
+    case KernelProcedureSignature.Mode.READ_ONLY => ProcReadOnly
+    case KernelProcedureSignature.Mode.READ_WRITE => ProcReadWrite
     case _ => throw new CypherExecutionException(
       "Unable to execute procedure, because it requires an unrecognized execution mode: " + mode.name(), null )
   }
